b840ec6519c378ede68ceea69b32d18103abb57b
bharat1704 <bharatmali1704@gmail.com>
2024-12-09 20:43:39 +0530

    created next-js app

---
+++ app/signin/layout.tsx
@@ -0,0 +1,10 @@
+
+export default function({ children }:{
+    children: React.ReactNode
+}) {
+return <div>
+<div className="border-b text-center">get 20 percent off </div>
+{ children }
+</div>
+
+}
